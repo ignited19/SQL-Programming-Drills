9aa571fa75d060b9f721d973c580bb0588a50e2f
ignited19 <38870344+ignited19@users.noreply.github.com>
2020-01-11 09:36:53 -0800

    Create Triangle.sql

---
+++ Triangle.sql
@@ -0,0 +1,13 @@
+/*=================================================
+Write a query identifying the type of Triangle 
+each record is in the TRIANGLES table using its 
+three side lengths. 
+==================================================*/
+
+SELECT CASE 
+    WHEN (A+B<=C) OR (C+B<=A) OR (A+C<=B) THEN 'Not A Triangle'
+    WHEN A=B AND B=C THEN 'Equilateral'
+    WHEN A=B OR B=C OR A=C THEN 'Isosceles'
+    ELSE 'Scalene'
+END 
+FROM Triangles
